35e1d276275cd51013102e126d23faf37bead2a8
vasilevl <vasilevl@TBEA311-24>
2017-10-31 14:24:01 -0700

    played around with ajax

--- NodejsWebApp1/NodejsWebApp1/server.js
+++ NodejsWebApp1/NodejsWebApp1/server.js
@@ -1,3 +1,4 @@
+var express = require('express');
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
@@ -12,160 +13,241 @@ http.createServer(function (req, res) {
     var body;
 
 
-    if (req.method == 'POST') {
-        console.log("state1?");
+    if (req.method == 'GET') {
 
         state = 4;
-        req.on('data', function (data) {
-            body += data;
-            console.log(body);
-            res.end();
+        console.log("hiii");
+
+
+
+
+
+    }
+
+
+    /*
+            req.on('end', function () {
+            var post = querystring.parse(body);
+            console.log(post);
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Hello World\n');
         });
-
-    } else {
-
-
-        /*
-                req.on('end', function () {
-                var post = querystring.parse(body);
-                console.log(post);
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end('Hello World\n');
-            });
-            */
-
-
-
-
-
-
-        //----------------------------------------------------------URL parsing //----------------------------------------------------------
-        var q = url.parse(req.url, true);
-        var filename = q.pathname;
-        //./ddd&something=val1&some2=val2
-        console.log(filename);
-
-        var tokens = "";
-        tokens = filename.split("&");
-        console.log(tokens);
-
-        //----------------------------------------------------------Return Pow of passed data in url //----------------------------------------------------------
-        if (tokens[0].includes("username")) {
-            //ddd, username=val1, pw=val2
-            var uName;
-            var pW;
-            var pwFinal;
-            var uNameFinal;
-            //resturantid=123&mitem1=23&mmitem2=23&mitem3=32&extra1=0&extra2=0
-            if (tokens.length > 1) {
-                uName = tokens[0].split("username=");
-                //uNameFinal = val1;
-                uNameFinal = uName[1];
-            }
-            if (tokens.length >= 2) {
-                pW = tokens[1].split("pw=");
-                //pwFinal = val2;
-                pwFinal = pW[1];
-            }
-
-            var pwInt = parseInt(pwFinal);
-            pwInt = Math.pow(pwInt, pwInt);
-
-            console.log(uNameFinal + " final username");
-            console.log(pwFinal + " final password");
-            console.log("-----------------------------------");
-            //var s = lookupUser(uNameFinal);
-
-            //console.log(s.toString() + "ourvalue");
-
-            /*	res.write(uNameFinal);
-           res.write(pwFinal);
-        if (filename.search("ddd") > 0) {
-                  res.writeHead(200, { 'Content-Type': 'text/html' });
-                   res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-                   res.write('<input type="file" name="filetoupload"><br>');
-                   res.write('<input type="submit">');
-                   res.write('</form>');
-                   // res.write('<img src = "animeguylol.JPG" style="width:304px;height:228px;">')
-           
-        }
         */
-            res.write(pwInt.toString());
+
+
+
+
+
+
+    //----------------------------------------------------------URL parsing //----------------------------------------------------------
+    var q = url.parse(req.url, true);
+    var filename = q.pathname;
+    //./ddd&something=val1&some2=val2
+    console.log(filename);
+
+    var tokens = "";
+    tokens = filename.split("&");
+    console.log(tokens);
+
+    //----------------------------------------------------------Return Pow of passed data in url //----------------------------------------------------------
+    if (tokens[0].includes("username")) {
+        //ddd, username=val1, pw=val2
+        var uName;
+        var pW;
+        var pwFinal;
+        var uNameFinal;
+        //resturantid=123&mitem1=23&mmitem2=23&mitem3=32&extra1=0&extra2=0
+        if (tokens.length > 1) {
+            uName = tokens[0].split("username=");
+            //uNameFinal = val1;
+            uNameFinal = uName[1];
+        }
+        if (tokens.length >= 2) {
+            pW = tokens[1].split("pw=");
+            //pwFinal = val2;
+            pwFinal = pW[1];
+        }
+
+        var pwInt = parseInt(pwFinal);
+        pwInt = Math.pow(pwInt, pwInt);
+
+        console.log(uNameFinal + " final username");
+        console.log(pwFinal + " final password");
+        console.log("-----------------------------------");
+        //var s = lookupUser(uNameFinal);
+
+        //console.log(s.toString() + "ourvalue");
+
+        /*	res.write(uNameFinal);
+       res.write(pwFinal);
+    if (filename.search("ddd") > 0) {
+              res.writeHead(200, { 'Content-Type': 'text/html' });
+               res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+               res.write('<input type="file" name="filetoupload"><br>');
+               res.write('<input type="submit">');
+               res.write('</form>');
+               // res.write('<img src = "animeguylol.JPG" style="width:304px;height:228px;">')
+       
+    }
+    */
+        res.write(pwInt.toString());
+        res.end();
+
+
+    }
+    //---------------------------------------------------------- send basic html  //----------------------------------------------------------
+    for (var i = 0; i < tokens.length; i++) {
+        if (tokens[i].includes("url3")) {
+            res.write("<html>");
+            res.write("<img src =  https://i.imgur.com/4IG8ITg.jpg>");
+            res.write("");
+            res.write("");
+            res.write("");
+            res.write("");
+            res.write("");
+            res.write("");
+            res.write("</html>");
+            res.write("<img src =  https://i.imgur.com/4IG8ITg.jpg>");
             res.end();
-
-
+            break;
         }
-        for (var i = 0; i < tokens.length; i++) {
-            if (tokens[i].includes("url3")) {
-                res.write("<html>");
-                res.write("<img src =  https://i.imgur.com/4IG8ITg.jpg>");
-                res.write("");
-                res.write("");
-                res.write("");
-                res.write("");
-                res.write("");
-                res.write("");
-                res.write("</html>");
-                res.write("<img src =  https://i.imgur.com/4IG8ITg.jpg>");
-                res.end();
-                break;
+    }
+    var flag = 0;
+    //----------------------------------------------------------just sending some more html  //----------------------------------------------------------
+    for (var i = 0; i < tokens.length; i++) {
+        if (tokens[i].includes("website")) {
+            /*
+            //!!!!!!!!!FILE READING NEEDS TO BE FIXED!!!!!!!!!!
+            var n = 0;
+
+
+            if (n == 0) {
+
+                fs.readFile('html1.html', function (err, data) {
+                    console.log(data);
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(data);
+                    res.end();
+                });
+
             }
         }
-        var flag = 0;
-        for (var i = 0; i < tokens.length; i++) {
-            if (tokens[i].includes("website")) {
-                /*
-                //!!!!!!!!!FILE READING NEEDS TO BE FIXED!!!!!!!!!!
-                var n = 0;
-
-
-                if (n == 0) {
-
-                    fs.readFile('html1.html', function (err, data) {
-                        console.log(data);
-                        res.writeHead(200, { 'Content-Type': 'text/html' });
-                        res.write(data);
-                        res.end();
-                    });
-
-                }
-            }
-            
-            */
-                res.write("<html>");
-                res.write("<html>");
-                res.write("<body>");
-                res.write("<form    method='POST'>");
-                res.write("First name:<br>");
-                res.write("<input type='text' name='firstname' value='mickey'>");
-                res.write("<br>");
-                res.write("Last name:<br>");
-                res.write("<input type='submit' value='Submit'>");
-                res.write("</form>");
-                res.write("</body>");
-                res.write("</html>");
-                res.write("");
-                res.write("");
-                res.write("");
-                res.write("");
-                res.write("");
-                res.write("<img src =  https://i.imgur.com/4IG8ITg.jpg>");
+        
+        */
+            res.write("<html>");
+            res.write("<html>");
+            res.write("<body>");
+            res.write("<form    method='POST'>");
+            res.write("First name:<br>");
+            res.write("<input type='text' name='firstname' value='mickey'>");
+            res.write("<br>");
+            res.write("Last name:<br>");
+            res.write("<input type='submit' value='Submit'>");
+            res.write("</form>");
+            res.write("</body>");
+            res.write("</html>");
+            res.write("");
+            res.write("");
+            res.write("");
+            res.write("");
+            res.write("");
+            res.write("<img src =  https://i.imgur.com/4IG8ITg.jpg>");
 
 
 
 
-                res.end();
-                break;
-
-            }
-
+            res.end();
+            break;
 
         }
 
 
     }
+
+    //----------------------------------------------------------AJAX! //----------------------------------------------------------
+    for (var i = 0; i < tokens.length; i++) {
+        if (tokens[i].includes("aja")) {
+            /*
+            //!!!!!!!!!FILE READING NEEDS TO BE FIXED!!!!!!!!!!
+            var n = 0;
+
+
+            if (n == 0) {
+
+                fs.readFile('html1.html', function (err, data) {
+                    console.log(data);
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(data);
+                    res.end();
+                });
+
+            }
+        }
+        
+      
+            res.write("<html>");
+            res.write("<html>");
+            res.write("<body>");
+            res.write("<form    method='POST'>");
+            res.write("First name:<br>");
+            res.write("<input type='text' name='firstname' value='mickey'>");
+            res.write("<br>");
+            res.write("Last name:<br>");
+            res.write("<input type='submit' value='Submit'>");
+            res.write("</form>");
+            res.write("</body>");
+            res.write("</html>");
+            res.write("");
+            res.write("");
+            res.write("");
+            res.write("");
+            res.write("");
+            res.write("<img src =  https://i.imgur.com/4IG8ITg.jpg>");
+
+
+              
+
+          
+            */
+            res.write("<!DOCTYPE html>");
+            res.write("<html>");
+            res.write("<body>");
+            res.write("");
+            res.write("<div id='demo'>");
+            res.write("<h1>The XMLHttpRequest Object</h1>");
+            res.write("<button type='button' onclick='loadDoc()'>Change Content</button>");
+            res.write("</div>");
+            res.write("");
+            res.write("<script>");
+            res.write("function loadDoc() {");
+            res.write("var xhttp = new XMLHttpRequest();");
+            res.write("xhttp.onreadystatechange = function() {");
+            res.write("if (this.readyState == 4 && this.status == 200) {");
+            res.write("document.getElementById('demo').innerHTML =");
+            res.write("this.responseText;");
+            res.write("}");
+            res.write("};");
+            res.write("xhttp.open('GET', 'u=5&aja', true);");
+            res.write("xhttp.send(u=5&aja);");
+            res.write("}");
+            res.write("</script>");
+            res.write("");
+            res.write("</body>");
+            res.write("</html>");
+
+
+
+            res.end();
+            break;
+
+        }
+
+
+    }
+
+
     //---------------------------------------------------------- TEXT //----------------------------------------------------------
-    console.log(state.toString());
+
 
 
 
@@ -187,13 +269,11 @@ http.createServer(function (req, res) {
        return res.end();
      });
      */
-}).listen(81);
+}).listen(1337);
 
 server.on('request', function (req, res) { //'request'
 
 });
-
-
 
 
 
